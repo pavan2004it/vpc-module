d1bb19afc2db6cafff261c094b2fc90db9cd86f7
Pavan Kumar <pavan_2004it@icloud.com>
2023-09-14 17:09:57 +0530

    added nat gateway

--- main.tf
+++ main.tf
@@ -719,7 +719,7 @@ resource "aws_nat_gateway" "rp-nat" {
 }
 
 resource "aws_route" "nat_route" {
-  route_table_id = aws_route_table.private_route_table[*].id
+  route_table_id = aws_route_table.private_route_table[0].id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.rp-nat.id
 }
